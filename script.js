e1b917f44231a44d9a0326fc2023eb800fc3e4f1
inesdpo <117300107+inesdpo@users.noreply.github.com>
2026-01-16 19:42:27 +0000

    initialcommit

---
+++ script.js
@@ -0,0 +1,61 @@
+const players = ['X', 'O'];
+let currentPlayer = 0;
+let popup = document.getElementById('popup');
+
+const currentPlayerSpan = document.getElementById('currentPlayer');
+const winnerSpan = document.getElementById('winner');
+const cards = document.querySelectorAll(".card img");
+
+function turnCard(card) {
+    const img = card.querySelector("img");
+    if (img.src.includes('images/cross.png') || img.src.includes('images/circle.png')) {
+        return; 
+    }
+    if (currentPlayer == 0) {
+        img.src = 'images/cross.png';
+    } 
+    if (currentPlayer == 1 ) {
+        img.src = 'images/circle.png';
+    }
+
+    img.style.display = "block";
+   
+    currentPlayer = (currentPlayer + 1) % players.length;
+    win();
+    currentPlayerSpan.textContent = players[currentPlayer];
+    
+}
+
+function resetGame() {
+    cards.forEach(img => {
+        img.src = '';
+        img.style.display = "none";
+    });
+    currentPlayer = 0;
+    currentPlayerSpan.textContent = players[currentPlayer];
+}
+
+function win() {
+
+    if ((cards[0].src.includes('cross.png') || cards[0].src.includes('circle.png')) && cards[0].src === cards[1].src && cards[1].src === cards[2].src ||
+        (cards[3].src.includes('cross.png') || cards[3].src.includes('circle.png')) && cards[3].src === cards[4].src && cards[4].src === cards[5].src ||
+        (cards[6].src.includes('cross.png') || cards[6].src.includes('circle.png')) && cards[6].src === cards[7].src && cards[7].src === cards[8].src ||
+        (cards[0].src.includes('cross.png') || cards[0].src.includes('circle.png')) && cards[0].src === cards[3].src && cards[3].src === cards[6].src ||
+        (cards[1].src.includes('cross.png') || cards[1].src.includes('circle.png')) && cards[1].src === cards[4].src && cards[4].src === cards[7].src ||
+        (cards[2].src.includes('cross.png') || cards[2].src.includes('circle.png')) && cards[2].src === cards[5].src && cards[5].src === cards[8].src ||
+        (cards[0].src.includes('cross.png') || cards[0].src.includes('circle.png')) && cards[0].src === cards[4].src && cards[4].src === cards[8].src ||
+        (cards[2].src.includes('cross.png') || cards[2].src.includes('circle.png')) && cards[2].src === cards[4].src && cards[4].src === cards[6].src) {
+            winnerSpan.textContent = players[(currentPlayer + 1) % players.length];
+            openPopup();
+    }   
+
+}
+
+function openPopup() {
+    popup.classList.add('open-popup');
+
+}
+
+function closePopup() {
+    popup.classList.remove('open-popup');
+}
